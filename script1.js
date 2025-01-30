8af52cd4d216832d9230a0d44c9a9a83403bb66e
akhil1225 <akhilendra631@gmail.com>
2025-01-30 11:38:58 +0530

    Named and Default Exports

---
+++ script1.js
@@ -0,0 +1,8 @@
+let msg = "hello, good morning!!!";
+
+function greet(){
+    return msg + " I am coding now";
+}
+
+export {msg};
+export default greet;
---
+++ script2.js
@@ -0,0 +1,5 @@
+import  {msg as myMsg}  from "./script1.js";
+import  greet  from "./script1.js";
+
+console.log(myMsg);
+console.log(greet());
