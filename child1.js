313af329152ec56fe6a2b230d04bed19430464cd
akhil1225 <akhilendra631@gmail.com>
2025-01-31 11:44:48 +0530

    Named and Default imports and exports

---
+++ child1.js
@@ -0,0 +1,28 @@
+// let userDeets = {
+//     name: "Akhil",
+//     profession : "Developer"
+// }
+
+// export default userDeets;
+
+let x = 10;
+
+const user = "Tanuja";
+
+const users = ["satwick", "lokesh", "harsha", "koushik", "mahesh"]
+
+const obj = {
+    name: "sam",
+    company : "excelR"
+}
+
+export {
+    x,
+    user,
+    users, 
+    obj
+}
+ 
+let college = "Mallareddy University";
+
+export default college;
