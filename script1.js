bc0b55e16ea83c855f5908d421fc0d518666e69a
akhil1225 <akhilendra631@gmail.com>
2025-02-25 11:43:08 +0530

    CSS Grid Layout

--- script1.js
+++
@@ -1,8 +0,0 @@
-let msg = "hello, good morning!!!";
-
-function greet(){
-    return msg + " I am coding now";
-}
-
-export {msg};
-export default greet;
--- script2.js
+++
@@ -1,5 +0,0 @@
-import  {msg as myMsg}  from "./script1.js";
-import  greet  from "./script1.js";
-
-console.log(myMsg);
-console.log(greet());
